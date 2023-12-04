3af46842c9c5f993f9791db46d92d09c999034e1
Shashikant-15 <shashikant.tanti@nashtechglobal.com>
2023-12-04 13:37:57 +0530

    Add project files.

---
+++ xunitExample.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xunitExample.Controllers;
+
+namespace xunitExample.Tests.Controllers
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void HomeController_Index_ValidReult() 
+        {
+            // AAA Methodology
+            // Arrange
+
+            HomeController controller = new HomeController();
+            string expectedResult = "I am in Home";
+
+            //Act
+           string result = controller.Index();
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+
+        }
+    }
+}
---
+++ xunitExample/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace xunitExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "I am in Home";
+        }
+    }
+}
